e9023ab7f1a1bb0d141e81bd866d8e1c901dd298
Kirill Bogatikov <kivi15penza@gmail.com>
2019-12-09 00:17:08 +0300

    little javadoc

--- log/src/main/java/org/cuba/log/Level.java
+++ log/src/main/java/org/cuba/log/Level.java
@@ -1,5 +1,12 @@
 package org.cuba.log;
 
+/**
+ * Represents levels of log record 
+ * 
+ * @author Kirill Bogatikov
+ * @version 1.0
+ * @since 1.0.0
+ */
 public enum Level {
     INFO, WARN, ERROR, DEBUG
 }
