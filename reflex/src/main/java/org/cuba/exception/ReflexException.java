ce5df8264cf71dd8704f59e06d3623110cf6f3e1
Kirill Bogatikov <kivi15penza@gmail.com>
2019-12-18 02:06:21 +0300

    Added comments for exceptions package and ReflexException class

--- reflex/src/main/java/org/cuba/exception/ReflexException.java
+++ reflex/src/main/java/org/cuba/exception/ReflexException.java
@@ -1,47 +1,66 @@
 package org.cuba.exception;
 
 /**
+ * Represents 'abstract' reflex exception, occurred at operation<br>
+ * with Java reflection API or Cuba Reflex API
+ * 
  * @author Kirill Bogatikov
- * @version 1.0
+ * @version 1.0.1
  * @since 1.0
  */
 public class ReflexException extends RuntimeException {
     private static final long serialVersionUID = -794179139780556345L;
 
     /**
+     * Default constructor. Creates empty exception without message, but with stack trace
      * 
+     * @see {@link RuntimeException#RuntimeException() default contructor of RuntimeException}
      */
     public ReflexException() {
         super();
     }
 
     /**
-     * @param message
+     * Creates exception with specified message and stack trace
+     *  
+     * @see {@link RuntimeException#RuntimeException(String) message contructor of RuntimeException}
+     * 
+     * @param message string message for exception
      */
     public ReflexException(String message) {
         super(message);
     }
 
     /**
-     * @param cause
+     * Creates exception caused by specified {@link Throwable}
+     * 
+     * @see {@link RuntimeException#RuntimeException(Throwable) contructor of RuntimeException}
+     * 
+     * @param cause 'parent' exception of this
      */
     public ReflexException(Throwable cause) {
         super(cause);
     }
 
     /**
-     * @param message
-     * @param cause
+     * Creates exception caused by specified {@link Throwable} with<br>
+     * specified message
+     * 
+     * @param message string message for exception
+     * @param cause 'parent' exception of this
      */
     public ReflexException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * @param message
-     * @param cause
-     * @param enableSuppression
-     * @param writableStackTrace
+     * Creates exception caused by specified {@link Throwable} with<br>
+     * specified message
+     * 
+     * @param message string message for exception
+     * @param cause 'parent' exception of this
+     * @param enableSuppression whether or not suppression is enabled or disabled
+     * @param writableStackTrace whether or not the stack trace should be writable
      */
     public ReflexException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
--- reflex/src/main/java/org/cuba/exception/package-info.java
+++ reflex/src/main/java/org/cuba/exception/package-info.java
@@ -1,6 +1,12 @@
 /**
+ * Contains exceptions for Reflex
+ * 
+ * <ul>
+ *     <li>{@link org.cuba.exception.ReflexException Reflex Runtime Exception}</li>
+ * </ul>
+ * 
  * @author Kirill Bogatikov
  * @version 1.0
- * @since 1.0 *
+ * @since 1.1.0
  */
 package org.cuba.exception;
